24814d33d1510f15ae79baba124f1479e6c37209
Anton Ocheredko <agents@mail.ru>
2019-05-15 08:18:20 +0300

    1.3. JavaDoc. Code convention[#140538]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,3 +1,11 @@
+/**
+* Package for calculate task.
+*
+* @author Anton Ocheredko (agents@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
 package ru.job4j.calculate;
 
 public class Calculate {
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Anton Ocheredko (agents@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j.calculate;
