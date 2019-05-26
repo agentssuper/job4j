53db79cedfa6a21423a0015f14c0c5590c08ebea
Anton Ocheredko <agents@mail.ru>
2019-05-27 01:23:51 +0300

    6.4. Слово начинается с ...[#140514]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинается с префикса
+     */
+    public boolean startsWith(String word, String prefix) {
+        boolean result = true;
+        char[] pref = prefix.toCharArray();
+        char[] wrd = word.toCharArray();
+		for (int index = 0; index < pref.length; index++){
+			if (wrd[index] != pref [index]){
+				result = false;
+				break;
+			}
+		}	
+		      return result;
+    }
+}
