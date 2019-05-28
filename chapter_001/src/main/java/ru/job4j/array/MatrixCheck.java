b9a712d0c258d16adc2c910ab1cffcd05f6dfb80
Anton Ocheredko <agents@mail.ru>
2019-05-28 20:17:50 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#140516]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -6,6 +6,7 @@ public class MatrixCheck {
         for (int x = 0; x < data.length - 1; x++) {
              if (data[x][x] != data[x + 1] [x + 1] || data [x][data.length - 1 - x] != data [x + 1][data.length - 1 - (x + 1)]) {
                     result = false;
+                    break;
                 }
             }
         return result;
