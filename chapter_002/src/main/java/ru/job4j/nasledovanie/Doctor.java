04c18811d52889b922a2f0990d9dfe703c267674
Anton Ocheredko <agents@mail.ru>
2019-06-09 14:38:25 +0300

    1. Реализация профессий в коде[#140609]

--- chapter_002/src/main/java/ru/job4j/nasledovanie/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/nasledovanie/Doctor.java
@@ -1,6 +1,13 @@
-public class extends Profession{
+public class Doctor extends Profession{
 	private String diagnose;
 	public diagnoseheal(Pacient pacient){
 		//Ставит диагноз
+	}
+	public Doctor(String name, String surname, String education, String birthday, String diagnose){
+		this.name = name;
+		this.surname = surname;
+		this.birthday = birthday;
+		this.education = education;
+		this.diagnose = diagnose;
 	}
 }
